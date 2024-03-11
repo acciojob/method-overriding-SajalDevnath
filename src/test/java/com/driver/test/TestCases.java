484e41d473d0c99273d09bc9cc37761d5bd1f1f4
SajalDevnath <sajaldevnath96@gmail.com>
2024-03-11 17:09:40 +0530

    Test cases added

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,11 +1,28 @@
 package com.driver.test;
 
-import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import com.driver.A;
 import com.driver.B;
 
 public class TestCases {
-    
+
+    @Test
+    public void testMethodFromA() {
+        A objA = new A();
+        assertEquals("Invoking method from class A", objA.meth());
+    }
+
+    @Test
+    public void testOverriddenMethodFromB() {
+        B objB = new B();
+        assertEquals("Method is overridden in Extended class B", objB.meth());
+    }
+
+    @Test
+    public void testMethodFromAUsingBObject() {
+        A objA = new B(); // Polymorphism
+        assertEquals("Method is overridden in Extended class B", objA.meth());
+    }
 }
+
